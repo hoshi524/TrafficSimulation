9a0cbb36fab4fa9511c62fd18112ec9c0934d7d8
hoshi <hoshi524@gmail.com>
2018-08-27 00:52:17 +0900

    balance

--- src/Output.java
+++ src/Output.java
@@ -54,13 +54,17 @@ public class Output {
         }
         Type[][] types = new Type[N][N];
         for (int i = 0; i < roads.length; ++i) {
-            if (roads[i].length != 3) throw new VerifyException();
+            if (roads[i].length != 3)
+                throw new VerifyException();
             int a = roads[i][0];
             int b = roads[i][1];
             int c = roads[i][2];
-            if (a < 0 || N <= a) throw new VerifyException();
-            if (b < 0 || N <= b) throw new VerifyException();
-            if (a == b) throw new VerifyException();
+            if (a < 0 || N <= a)
+                throw new VerifyException();
+            if (b < 0 || N <= b)
+                throw new VerifyException();
+            if (a == b)
+                throw new VerifyException();
             Type t = Type.values()[c];
             types[a][b] = types[b][a] = t;
             cost += (t == Type.road ? input.roadCost : input.railCost) * dist(points[a], points[b]);
@@ -72,12 +76,14 @@ public class Output {
             for (int j = 0; j < paths[i].length; ++j) {
                 int n = paths[i][j];
                 Type t = types[p][n];
-                if (t == null) throw new VerifyException();
+                if (t == null)
+                    throw new VerifyException();
                 ++count[p][n];
                 ++count[n][p];
                 p = n;
             }
-            if (p != input.toPoint[i]) throw new VerifyException();
+            if (p != input.toPoint[i])
+                throw new VerifyException();
         }
         for (int i = 0; i < input.N; ++i) {
             int p = i;
@@ -90,13 +96,15 @@ public class Output {
                     d += dist(points[p], points[n]) * Math.sqrt(count[p][n]);
                 } else {
                     car = false;
-                    d += dist(points[p], points[n]) / 2;
+                    d += dist(points[p], points[n]) / 3;
                 }
                 p = n;
             }
-            if (car) d /= 2;
+            if (car)
+                d /= 2;
             time += d * d;
         }
+        time = Math.sqrt(time);
     }
 
     public double getScore() {
